63f31db00bc4496645a3c787ecc60a77f12b14d2
batawenam <alikhaliq66@gmail.com>
2022-05-23 13:08:36 +0100

    Add orientation params

--- lib/pdf.interfaces.ts
+++ lib/pdf.interfaces.ts
@@ -94,7 +94,7 @@ export interface ViewPortSize {
 }
 
 export interface PDFOptions extends CreateOptions {
-    orientation: string,
+    orientation?: string,
     viewportSize?: ViewPortSize;
     locals?: Record<string, any>;
 }
